99dfa33f2a1a5f44bc1a880cb50fcb2251ff8364
eng-nihad <hodzicnihad10@gmail.com>
2023-10-07 21:33:30 +0200

    Finished step 35:  Responsiveness

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
     "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
   ],
-  function (Controller, UIComponent, History) {
+  function (Controller, UIComponent, History, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
       onInit: function () {
@@ -14,6 +15,7 @@ sap.ui.define(
           .attachPatternMatched(this._onObjectMatched, this);
       },
       _onObjectMatched: function (oEvent) {
+        this.byId("rating").reset();
         this.getView().bindElement({
           path:
             "/" +
@@ -33,6 +35,16 @@ sap.ui.define(
           var oRouter = UIComponent.getRouterFor(this);
           oRouter.navTo("overview", {}, true);
         }
+      },
+      onRatingChange: function (oEvent) {
+        var fValue = oEvent.getParameter("value");
+        var oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+
+        MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
       },
     });
   }
